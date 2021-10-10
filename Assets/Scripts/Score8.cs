7c11938e529b8d5d84093f4c6e5646be4fc27b95
BlewahMarjan <muhammadyayang1@gmail.com>
2021-10-10 20:51:11 +0700

    challange8

---
+++ Assets/Scripts/Score8.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score8 : MonoBehaviour
+{
+
+    public float score = 0;
+    public Text scoreUI;
+    public Spawn SpawnObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("kotak"))
+        {
+            StartCoroutine(SpawnObject.SpawnObject());
+            score += 1;
+            scoreUI.text = score.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    
+
+    public GameObject objek1;
+    public Transform bola;
+    //public float radius;
+    private float posisix;
+    private float posisiy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("hasil "+objek1);
+       
+        int run = Random.Range(4, 10);
+        for (int i = 0; i < run; i++)
+        {
+            posisix = Random.Range(-8f, 8f);
+            posisiy = Random.Range(-4f, 4f);
+            transform.position = new Vector2(posisix, posisiy);
+            Instantiate(objek1, transform.position, Quaternion.identity);
+            
+        }
+        //InvokeRepeating("SpawnObject", 2.0f, 3.0f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("as y" + GameObject.FindGameObjectWithTag("bola").transform.position.y);
+        //Debug.Log("as x" + GameObject.FindGameObjectWithTag("bola").transform.position.x);
+
+    }
+
+ 
+
+    public IEnumerator SpawnObject()
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("halo");
+        posisix = Random.Range(-13f, 13f);
+        //Debug.Log(posisix);
+        posisiy = Random.Range(-8f, 8f);
+        // Debug.Log(posisiy);
+        //Debug.Log(GameObject.FindGameObjectWithTag("bola").transform.position.x);
+        //Debug.Log(GameObject.FindGameObjectWithTag("bola").transform.position.y);
+        //if ((posisix <= GameObject.FindGameObjectWithTag("bola").transform.position.x) || (posisiy <= GameObject.FindGameObjectWithTag("bola").transform.position.y)) 
+        //{
+        //posisix = Random.Range(-8f - 4 - GameObject.FindGameObjectWithTag("bola").transform.position.x, 8f + 4 + GameObject.FindGameObjectWithTag("bola").transform.position.x);
+        //posisiy = Random.Range(-4f - 4 - GameObject.FindGameObjectWithTag("bola").transform.position.y, 4f + 4 + GameObject.FindGameObjectWithTag("bola").transform.position.y);
+        //transform.position = new Vector2(posisix, posisiy);
+        //Instantiate(objek1, transform.position, Quaternion.identity);
+        //Debug.Log("if pertama");
+
+
+
+        //}
+        transform.position = new Vector2(posisix, posisiy);
+        if (Vector2.Distance(bola.transform.position, objek1.transform.position) < 2)
+        {
+            Debug.Log("halo2");
+            Instantiate(objek1, transform.position, Quaternion.identity);
+
+        }
+        else { Instantiate(objek1, transform.position, Quaternion.identity); }
+        
+    }
+    
+}
+
