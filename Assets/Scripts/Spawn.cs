425369d65920ee968f317ae7c72a8576a90dbe58
BlewahMarjan <muhammadyayang1@gmail.com>
2021-10-10 20:43:13 +0700

    Challange6

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject objek;
+    public float radius;
+    private float posisix;
+    private float posisiy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int ran = Random.Range(2, 10);
+        for (int i = 0; i < ran; i++)
+        {
+            posisix = Random.Range(-8f, 8f);
+            posisiy = Random.Range(-4f, 4f);
+            transform.position = new Vector2(posisix, posisiy);
+            Instantiate(objek, transform.position, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObject()
+    {
+        posisix = Random.Range(-8f, 8f);
+        posisiy = Random.Range(-4f, 4f);
+        transform.position = new Vector2(posisix, posisiy);
+        Instantiate(objek, transform.position, Quaternion.identity);
+    }
+}
+
